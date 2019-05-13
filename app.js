f2c561420b35759e7c7beba9ba8c3932b9f56486
sunkig <sunk.ig@aol.com>
2019-05-13 12:23:55 +0200

    fixed middleware, route and listen

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.static('public'));
+
+// Route
+app.get('/', function(req, res) {
+  res.send('Hello!');
+});
+
+// Listen
 app.listen(3000);
